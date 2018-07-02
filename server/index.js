d7cd6ba600d09dc14483f1d341e2593116f8d88b
jaimemendozadev <jaimemendozadev@gmail.com>
2018-07-02 13:07:07 -0700

    Add eslint and prettier to project

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
-const public = path.resolve(__dirname, '../public');
+
+const publicPath = path.resolve(__dirname, '../public');
 
 const bodyParser = require('body-parser');
 
-app.use(express.static(public))
+app.use(express.static(publicPath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.sendFile(public)
+  res.sendFile(publicPath);
 });
 
 module.exports = app;
-
-
