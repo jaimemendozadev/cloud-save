d0eba66dcee465f94b6493e9e6cc4a5127f970d5
jaimemendozadev <jaimemendozadev@gmail.com>
2018-07-06 17:06:41 -0700

    Fix Homepage rendering bug
    Homepage path was incorrect. So everytime the user
    was authenticated and taken to the ProtectedRoute
    to verify authentication, the PR component kept
    rendering the Loading component instead of the
    Homepage component due to the incorrect path
    typed in components/index.jsx.

--- dev/app/components/index.jsx
+++ dev/app/components/index.jsx
@@ -1,5 +1,5 @@
 export { default as SplashPage } from './SplashPage/index.jsx';
 export { default as SignIn } from './SignIn/index.jsx';
 export { default as Loading } from './Loading/index.jsx';
-export { default as Homepage } from './Loading/index.jsx';
+export { default as Homepage } from './Homepage/index.jsx'; // Incorrect Path caused error
 export { default as ProtectedRoute } from './ProtectedRoute/index.jsx';
