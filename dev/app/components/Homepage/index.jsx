88c1599a31ede25e8e65488a08f8dc5af4ace1cf
jaimemendozadev <jaimemendozadev@gmail.com>
2018-07-06 22:00:34 -0700

    Start worm on Homepage
    Homepage will have file input form and view
    that renders saved docs.

--- dev/app/components/Homepage/index.jsx
+++ dev/app/components/Homepage/index.jsx
@@ -4,15 +4,38 @@ import { connect } from 'react-redux';
 class Homepage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            currentFile: ''
+        }
+    }
+
+    handleFileUpload = event => {
+        console.log('event is ', event.target.files[0]);
+
+        this.setState({
+            currentFile: event.target.files[0]
+        })
+    }
+
+
+
+    handleSubmit = event => {
+
     }
 
     render() {
-        const { currentUser } = this.props
+        const { currentUser } = this.props;
+        const { currentFile } = this.state;
         console.log('currentUser is ', currentUser);
 
         return (
-            <div>
+            <div className='homepage'>
                 <h1>Welcome to the CloudSave Homepage</h1>
+                <form>
+                    <label>Choose file to upload</label>
+                    <input onChange={this.handleFileUpload} type='file' />
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
