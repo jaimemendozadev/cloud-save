8e01a859e8083774846902ba2225842fe511a732
jaimemendozadev <jaimemendozadev@gmail.com>
2018-07-09 10:01:23 -0700

    Add minor CSS styling

---
+++ dev/app/components/Document/utils.js
@@ -0,0 +1,9 @@
+const fileTypes = {
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document' : '.docx',
+    'application/msword': '.doc',
+    'application/pdf': '.pdf',
+    'image/gif': '.gif',
+    'image/jpeg': '.jpeg',
+    'image/png': '.png',
+    'image/svg+xml': '.svg'
+}
