1c387bb58039c09cde03486de681e05a0e11c3d3
jaimemendozadev <jaimemendozadev@gmail.com>
2018-07-09 12:04:11 -0700

    Add styling to Splash Page

--- dev/app/components/SplashPage/index.jsx
+++ dev/app/components/SplashPage/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SplashPage = () => (
-    <div>
-        <h1>Welcome to Cloud Save</h1>
-        <h2>A simple web app to store your files in the cloud</h2>
+    <div className='splash-page-container'>
+        <div className='splash-page-header'>
+            <h1>Cloud Save</h1>
+        </div>
+
+        <div className='splash-page-main'>
+            <div className='cloud-img-container'>
+                <img src='https://qph.fs.quoracdn.net/main-qimg-3f44012001f5d9c27d46a84e41f2d821' />
+            </div>
+            <div className='splash-page-verbiage'>
+                <h2>A simple web app to store your files in the cloud</h2>
+
+                <div className='splash-page-btn'>
+                    <Link to='/signin'>Sign In/Log In</Link>
+                </div>
+
+
+            </div>
+        </div>
     </div>
 )
 
